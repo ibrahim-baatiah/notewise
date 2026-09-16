08bfd859b59013b47686a7d5fa93e6bc16eacbc1
Ibrahim Omar <abra56heem@gmail.com>
2026-09-17 01:08:19 +0200

    Adding the Terraform Backend

---
+++ infra/providers.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  storage_use_azuread = true
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "Learning-RG"
+        storage_account_name = "tfblobcontainer"
+        container_name       = "terraform"
+        key                  = "terraform.tfstate"
+    }
+}
