fc527181df6bc8cf3edd7c91f83158143b1808c9
Ibrahim Omar <abra56heem@gmail.com>
2026-09-17 17:32:38 +0200

    Creating a VNET for the app #1 - FIX

--- infra/modules/network/outputs.tf
+++ infra/modules/network/outputs.tf
@@ -2,6 +2,6 @@ output "vnet_id" {
     value = azurerm_virtual_network.this.id
 }
 
-output "subnet_ids" {
-    value = { for s in azurerm_subnet.this : s.name => s.id }
-}
+# output "subnet_ids" {
+#     value = { for s in azurerm_subnet.this : s.name => s.id }
+# }
