1f6c286ac4895704ceeca49427091270a32f6d8f
Ibrahim Omar <abra56heem@gmail.com>
2026-09-21 00:58:59 +0200

    Ibrahim - Adding a destroy workflow - #1

--- infra/modules/acr/outputs.tf
+++ infra/modules/acr/outputs.tf
@@ -1,7 +1,8 @@
 output "acr_id" {
-  value = azurerm_azure_container_registry.acr.id
+  value = azurerm_container_registry.acr.id
 }
 
 output "acr_login_server" {
-  value = azurerm_azure_container_registry.acr.login_server
+  value = azurerm_container_registry.acr.login_server
 }
+
